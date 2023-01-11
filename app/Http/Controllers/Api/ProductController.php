4fbd0fc2bdd6163ffee272bf0d30b7139a852697
leiner2 <leinermenar@outlook.com>
2023-01-11 04:00:34 -0500

    correccion en el controlador

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Http\Controllers\Aoi;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
-class ProductoController extends Controller
+class ProductController extends Controller
 {
 
     public function index()
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\Aoi\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+//  ProductoController
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
